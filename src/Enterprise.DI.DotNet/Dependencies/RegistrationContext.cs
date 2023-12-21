49de0bd307020773f92a850b2ac4b99cbdf50f17
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2023-12-20 23:16:03 -0600

    Remove method, cleanup

--- src/Enterprise.DI.DotNet/Dependencies/RegistrationContext.cs
+++ src/Enterprise.DI.DotNet/Dependencies/RegistrationContext.cs
@@ -34,46 +34,44 @@ namespace Enterprise.DI.DotNet.Dependencies
 
         public IServiceCollection WithDecorator<TDecorator>() where TDecorator : class, TService
         {
-            return WithDecorator((serviceProvider, service) =>
-            {
-                return ActivatorUtilities.CreateInstance<TDecorator>(serviceProvider, service);
-            });
+            return WithDecorator((serviceProvider, service) => ActivatorUtilities.CreateInstance<TDecorator>(serviceProvider, service));
         }
 
         public IServiceCollection WithDecorator<TDecorator>(Func<IServiceProvider, TService, TDecorator> decoratorFactory) where TDecorator : class, TService
         {
-            (Type serviceType, ServiceLifetime lifetime) = GetServiceDescriptorDetails();
+            Type serviceType = typeof(TService);
 
-            // Capture the original service registration
+            // capture the original service registration
             ServiceDescriptor? originalServiceDescriptor = services
                 .FirstOrDefault(d => d.ServiceType == serviceType);
 
             if (originalServiceDescriptor == null)
-                throw new InvalidOperationException($"The original service of type {serviceType.Name} has not been registered.");
+                throw new InvalidOperationException($"The service of type {serviceType.Name} has not been registered and cannot be decorated.");
 
-            // Replace the original service with the decorator
+            ServiceLifetime lifetime = originalServiceDescriptor.Lifetime;
+
+            // replace the original service with the decorator
             object ImplementationFactory(IServiceProvider serviceProvider)
             {
                 TService originalService;
 
                 if (originalServiceDescriptor.ImplementationFactory != null)
                 {
-                    // Use the factory if available
+                    // use the factory if available
                     originalService = (TService)originalServiceDescriptor.ImplementationFactory(serviceProvider);
                 }
                 else if (originalServiceDescriptor.ImplementationInstance != null)
                 {
-                    // Use the instance if available
+                    // use the instance if available
                     originalService = (TService)originalServiceDescriptor.ImplementationInstance;
                 }
                 else if (originalServiceDescriptor.ImplementationType != null)
                 {
-                    // Create an instance using ActivatorUtilities if only a type is registered
+                    // create an instance using ActivatorUtilities if only a type is registered
                     originalService = (TService)ActivatorUtilities.CreateInstance(serviceProvider, originalServiceDescriptor.ImplementationType);
                 }
                 else
                 {
-                    // If none of the above, throw an exception as the service registration is not supported by this method
                     throw new InvalidOperationException("The registration method for the original service is not supported.");
                 }
 
@@ -81,22 +79,10 @@ namespace Enterprise.DI.DotNet.Dependencies
             }
 
             ServiceDescriptor decoratorDescriptor = ServiceDescriptor.Describe(serviceType, ImplementationFactory, lifetime);
+
             services.Replace(decoratorDescriptor);
 
             return services;
-        }
-
-        private (Type, ServiceLifetime) GetServiceDescriptorDetails()
-        {
-            Type serviceType = typeof(TService);
-
-            ServiceDescriptor? originalServiceDescriptor = services
-                .LastOrDefault(descriptor => descriptor.ServiceType == serviceType);
-
-            if (originalServiceDescriptor == null)
-                throw new InvalidOperationException($"Service type {serviceType.Name} has not been registered.");
-
-            return (serviceType, originalServiceDescriptor.Lifetime);
         }
     }
 }
