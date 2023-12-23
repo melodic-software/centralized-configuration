0cebfa3f1565543ea28e63e001ff31c982e7343e
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2023-12-23 12:49:20 -0600

    Update local Unix setup script

--- scripts/local/setup-unix.sh
+++ scripts/local/setup-unix.sh
@@ -1,17 +1,48 @@
 #!/bin/bash
-# Shell script for setting up the local development environment on Unix-like systems
 
-# Define database server and credentials - update these variables
-db_server="localhost"
-db_user="yourUsername"
-db_password="yourPassword"
+# Stop the script on the first error and treat unset variables as an error
+set -eu
+set -o pipefail
+
+# Function to exit with a message
+exit_with_message() {
+    local message="$1"
+    echo "$message"
+    read -p "Press Enter to exit"
+    exit 1
+}
+
+# Check if SQL Server tools are installed
+if ! command -v sqlcmd &> /dev/null; then
+    echo "Installing SQL Server command-line tools..."
+    # TODO: Add commands to install SQL Server tools for Unix
+    # This can vary based on the Unix distribution
+    echo "SQL Server command-line tools installation completed."
+fi
+
+# Define database server
+DB_SERVER="localhost"  # or ".\\SQLEXPRESS"
 
 # Path to the database initialization script
-db_init_script_path="../data/init/CreateDatabase.sql"
+DB_INIT_SCRIPT_PATH="../../data/init/CreateDatabase.sql"
 
-# Run the database initialization script using SQL Server authentication
-sqlcmd -S "$db_server" -U "$db_user" -P "$db_password" -i "$db_init_script_path"
+# Create a connection string for Windows Authentication with TrustServerCertificate
+# Note: Authentication might differ in Unix environments
+CONNECTION_STRING="Server=${DB_SERVER};Database=master;Integrated Security=True;TrustServerCertificate=True;"
+
+# Run the database initialization script with Windows Authentication
+if ! sqlcmd -S "${DB_SERVER}" -d "master" -i "${DB_INIT_SCRIPT_PATH}"; then
+    exit_with_message "An error occurred during database initialization"
+fi
+
+# Baseline the database (only needed if the schema is not empty)
+# Uncomment the next line if you need to baseline
+# flyway -configFiles="../../data/flyway/flyway.toml" -environment=local baseline
 
 # Run Flyway migrations
-# Ensure that the Flyway CLI is installed and configured correctly
-flyway migrate
+if ! flyway -configFiles="../../data/flyway/flyway.toml" -environment=local migrate; then
+    exit_with_message "An error occurred during Flyway migration"
+fi
+
+echo "Setup completed successfully. Press Enter to exit."
+read -p "Press Enter to exit"
