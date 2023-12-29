ee234cfead8ac46e6556c9d3f09566e95df9fa5e
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2023-12-28 23:57:39 -0600

    Add NLog to example project

--- examples/CompanyEmployees/Program.cs
+++ examples/CompanyEmployees/Program.cs
@@ -1,9 +1,16 @@
+using Contracts;
 using Enterprise.API;
+using LoggerService;
 
 ApiConfigurationService.Configure(args, options =>
 {
     options.HttpRequestMiddlewareOptions.AddCustomMiddleware = app =>
     {
         
+    };
+
+    options.ServiceConfigurationOptions.RegisterCustomServices = (services, builder) =>
+    {
+        services.AddSingleton<ILoggerManager, LoggerManager>();
     };
 });
