5a11257eded8fbacd9b864e59a1ca149f770c4f1
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2023-12-29 10:47:31 -0600

    Add more example code

--- examples/CompanyEmployees/Program.cs
+++ examples/CompanyEmployees/Program.cs
@@ -1,6 +1,10 @@
 using Contracts;
 using Enterprise.API;
 using LoggerService;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+using Service;
+using Service.Contracts;
 
 ApiConfigurationService.Configure(args, options =>
 {
@@ -12,5 +16,10 @@ ApiConfigurationService.Configure(args, options =>
     options.ServiceConfigurationOptions.RegisterCustomServices = (services, builder) =>
     {
         services.AddSingleton<ILoggerManager, LoggerManager>();
+        services.AddScoped<IRepositoryManager, RepositoryManager>();
+        services.AddScoped<IServiceManager, ServiceManager>();
+
+        services.AddDbContext<RepositoryContext>(opts =>
+            opts.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
     };
 });
---
+++ examples/Contracts/IRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace Contracts;
+
+// NOTE: This has some leaked abstraction with "trackChanges".
+// Also, not everything is going to support IQueryable<T>, which in itself exposes too much power to the client.
+// It allows for data access / queries to be made outside the repository reference and implementation.
+// Do not use this in real solutions.
+
+public interface IRepositoryBase<T>
+{
+    IQueryable<T> FindAll(bool trackChanges);
+    IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges);
+    void Create(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
