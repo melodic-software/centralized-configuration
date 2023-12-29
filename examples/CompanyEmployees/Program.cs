5faa63ec73295ea708733e08e4e45fa9dbb8f2e3
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2023-12-28 18:27:13 -0600

    Remove custom middleware

--- examples/CompanyEmployees/Program.cs
+++ examples/CompanyEmployees/Program.cs
@@ -4,9 +4,6 @@ ApiConfigurationService.Configure(args, options =>
 {
     options.HttpRequestMiddlewareOptions.AddCustomMiddleware = app =>
     {
-        app.Run(async context =>
-        {
-            await context.Response.WriteAsync("Hello from middleware component.");
-        });
+        
     };
 });
