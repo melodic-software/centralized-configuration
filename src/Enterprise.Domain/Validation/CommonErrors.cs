e199b4d5333b65ff75636ab2cbf5cc401a164790
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2023-12-31 14:28:30 -0600

    Add domain (enterprise) project

---
+++ src/Enterprise.Domain/Validation/CommonErrors.cs
@@ -0,0 +1,9 @@
+﻿namespace Enterprise.Domain.Validation
+{
+    public class CommonErrors
+    {
+        public static Error EntityNotFound = new(
+            "Entity.NotFound",
+            "The entity with the specified identifier was not found.");
+    }
+}
---
+++ src/Enterprise.Domain/Validation/Error.cs
@@ -0,0 +1,10 @@
+﻿namespace Enterprise.Domain.Validation;
+
+public class Error(string code, string message)
+{
+    public string Code { get; } = code;
+    public string Message { get; } = message;
+
+    public static Error None = new(string.Empty, string.Empty);
+    public static Error NullValue = new("Error.NullValue", "Null value was provided.");
+}
