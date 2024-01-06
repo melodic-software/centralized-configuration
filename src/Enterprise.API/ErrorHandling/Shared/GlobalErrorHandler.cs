cf7effb13729c3429c7844d7120b9a50612a2836
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-01-06 16:01:55 -0600

    Update global error handler (add alternative)

--- src/Enterprise.API/ErrorHandling/Shared/GlobalErrorHandler.cs
+++ src/Enterprise.API/ErrorHandling/Shared/GlobalErrorHandler.cs
@@ -1,8 +1,11 @@
-﻿using System.Net.Mime;
+﻿using System;
+using System.Net.Mime;
 using System.Text.Json;
 using Enterprise.API.ErrorHandling.Model;
+using Enterprise.Exceptions;
 using Enterprise.Logging.Events;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace Enterprise.API.ErrorHandling.Shared;
@@ -17,6 +20,15 @@ public class GlobalErrorHandler
         if (context.Response.HasStarted)
             return;
 
+        // These are two separate methods for handling the error.
+        // Since exception handlers or the Hellang middleware is more likely to be used I left this as is.
+
+        //await HandleError(context, exception, logger);
+        await HandleProblemDetails(context, exception);
+    }
+
+    private static async Task UseSimpleError(HttpContext context, Exception exception, ILogger logger)
+    {
         // TODO: Provide functionality to use JSON or XML depending on the request header.
 
         context.Response.StatusCode = StatusCodes.Status500InternalServerError;
@@ -29,6 +41,7 @@ public class GlobalErrorHandler
         EventId eventId;
 
         ErrorDetailsDto errorDetailsDto = new ErrorDetailsDto(context.Response.StatusCode, ErrorMessage);
+
         string json = JsonSerializer.Serialize(errorDetailsDto, jsonSerializerOptions);
 
         if (exception is HttpRequestException httpRequestException)
@@ -45,10 +58,51 @@ public class GlobalErrorHandler
         else
         {
             await context.Response.WriteAsync(json);
-             
+
             eventId = LogEventIds.UnknownError;
         }
 
         logger?.LogError(eventId, exception, exception.Message);
     }
+
+    private static async Task HandleProblemDetails(HttpContext context, Exception exception)
+    {
+        ExceptionDetails exceptionDetails = GetExceptionDetails(exception);
+
+        ProblemDetails problemDetails = new ProblemDetails
+        {
+            Status = exceptionDetails.Status,
+            Type = exceptionDetails.Type,
+            Title = exceptionDetails.Title,
+            Detail = exceptionDetails.Detail
+        };
+
+        if (exceptionDetails.Errors is not null)
+            problemDetails.Extensions["errors"] = exceptionDetails.Errors;
+
+        context.Response.StatusCode = exceptionDetails.Status;
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    }
+
+    private static ExceptionDetails GetExceptionDetails(Exception exception)
+    {
+        return exception switch
+        {
+            ValidationException validationException => new ExceptionDetails(
+                StatusCodes.Status400BadRequest,
+                "ValidationFailure",
+                "Validation Error",
+                "One or more validation errors has occurred.",
+                validationException.ValidationErrors),
+            _ => new ExceptionDetails(
+                StatusCodes.Status500InternalServerError,
+                "ServerError",
+                "Server Error",
+                "An unexpected error has occurred.",
+                null)
+        };
+    }
 }
+
+internal record ExceptionDetails(int Status, string Type, string Title, string Detail, IEnumerable<object>? Errors);
