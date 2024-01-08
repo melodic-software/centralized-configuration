1a821d821ea508d7bb0b6702c9a780d9ac3ea835
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-01-07 18:37:00 -0600

    Add architecture tests for assembly references

---
+++ tests/Configuration.Architecture.Tests/LayerTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace Configuration.Architecture.Tests
+{
+    public class LayerTests : BaseTest
+    {
+        [Fact]
+        public void DomainLayer_Should_NotHaveDependencyOn_ApplicationLayer()
+        {
+            TestResult? result = Types.InAssembly(DomainAssembly)
+                .Should()
+                .NotHaveDependencyOn(ApplicationServiceAssembly.GetName().Name)
+                .GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void DomainLayer_Should_NotHaveDependencyOn_InfrastructureLayer()
+        {
+            TestResult? result = Types.InAssembly(DomainAssembly)
+                .Should()
+                .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+                .GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ApplicationLayer_Should_NotHaveDependencyOn_InfrastructureLayer()
+        {
+            TestResult? result = Types.InAssembly(ApplicationServiceAssembly)
+                .Should()
+                .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+                .GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+    }
+}
